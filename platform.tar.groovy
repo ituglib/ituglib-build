cd845c3e79e52ab9f373da8fe067787c08ac0f9b
Randall S. Becker <rsbecker@nexbridge.com>
2017-12-01 13:21:37 -0600

    Added platform-specific packaging for basic structure.

---
+++ platform.tar.groovy
@@ -0,0 +1,23 @@
+/**
+ * Packager used for gz and bz2 files where the name has the form:
+ *    package-version-release.tar.(gz|bz2)
+ * that is repackaged with a different name replacing release with
+ * a suffix. So this would work for:
+ *    curl-7.46.0-release-nse.tar.gz
+ * shipping as
+ *    curl-7.46.0-nse.tar.gz
+ */
+import hudson.model.*;
+import java.sql.*;
+
+PlatformPackageEngine engine = new PlatformPackageEngine();
+JdbcLoader loader = new JdbcLoader();
+
+String schema = System.getenv("SCHEMA");
+engine.setSchema(schema);
+
+Connection connection = loader.getConnection();
+engine.setConnection(connection);
+
+engine.execute();
+connection.close();
