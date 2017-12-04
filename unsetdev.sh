3ac08b19fd6cb49ad86659720b77e23431d352fd
Randall S. Becker <rsbecker@nexbridge.com>
2017-12-04 10:23:53 -0600

    Added replace string to unsetdev.sh

--- unsetdev.sh
+++ unsetdev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Shell script to change the version string of a product to remove 
+# Shell script to change the version string of a product to remove or change
 
 Usage() {
-	echo "usage: $0 versionfile versionkey suffix"
+	echo "usage: $0 versionfile versionkey suffix [new-string]"
 	exit 1
 }
 
@@ -13,12 +13,24 @@ fi
 VERSIONFILE=$1
 VERSIONKEY=$2
 SUFFIX=$3
+REPLACE=$4
 
-ed $1 << FIXME
+if [ "$REPLACE" = "" ]; then
+	ed $1 << FIXME
 g/${VERSIONKEY}/s/${SUFFIX}//g
 g/${VERSIONKEY}/
 w
 q
 FIXME
+
+else
+	ed $1 << FIXME
+g/${VERSIONKEY}/s/${SUFFIX}/${REPLACE}/g
+g/${VERSIONKEY}/
+w
+q
+FIXME
+
+fi
 
 exit $?
