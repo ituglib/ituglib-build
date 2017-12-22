5b9c775c63fb33a32a4756f87d3750b9984689da
Randall S. Becker <rsbecker@nexbridge.com>
2017-12-22 13:30:19 -0600

    Added push for created tag in jenkins_tag.sh

--- jenkins_tag.sh
+++ jenkins_tag.sh
@@ -2,6 +2,7 @@
 
 echo Creating Jenkins tag in git ${GIT_URL}
 
+echo git tag -a --file=- jenkins/${BUILD_TAG} ${GIT_COMMIT}
 git tag -a --file=- jenkins/${BUILD_TAG} ${GIT_COMMIT} <<ANNOTATION
 url: ${BUILD_URL}
 job: ${JOB_NAME}
@@ -9,3 +10,7 @@ workspace: ${WORKSPACE}
 instance: ${JENKINS_URL}
 git: ${GIT_URL}
 ANNOTATION
+
+echo git push origin refs/tags/jenkins/${BUILD_TAG}:refs/tags/jenkins/${BUILD_TAG}
+git push origin refs/tags/jenkins/${BUILD_TAG}:refs/tags/jenkins/${BUILD_TAG}
+exit $?
