c82140aa848fc5c4d2aba5852364982d9cd7ebf3
Randall S. Becker <randall.becker@nexbridge.ca>
2018-01-03 07:41:10 -0800

    Modified manual packaging scripts.

--- ManualPackage.sh
+++ ManualPackage.sh
@@ -1,12 +1,59 @@
 #!/bin/sh
 
-set -x -v
+PROGNAME=$0
 
-export BASENAME=git
-export VERSION_PATH=/usr/local/bin/git
+Usage() {
+	echo "Usage: $PROGNAME base-name ( nse | nsx ) [-v version-file]"
+}
+
+if [ "$1" = "" -o "$2" = "" ]; then
+	Usage
+	exit 1
+fi
+
+export BASENAME=$1
+shift
+export VERSION_PATH=/usr/local/bin/$BASENAME
 export DEST=/home/ituglib/randall/stage
-export MANIFEST=git.bin.list
+export MANIFEST=${BASENAME}.bin.list
 export INSTALL_LOCATION=/usr/local
+export SYSTYPE=$1
+shift
+
+case $SYSTYPE in
+	nse)
+		echo "Packaging for NSE (H/J-series) systems."
+		;;
+	nsx)
+		echo "Packaging for NSX (L-series) systems."
+		;;
+	*)
+		echo "SYSTYPE $SYSTYPE is invalid. Must be nse or nsx."
+		exit 1
+		;;
+esac
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-v)
+			shift
+			if [ "$1" = "" ]; then
+				Usage
+				exit 1
+			fi
+			export VERSION_PATH=/usr/local/bin/$1
+			;;
+		*)
+			Usage
+			exit 1
+	esac
+	shift
+done
+echo Using BASENAME=$BASENAME
+echo Using VERSION_PATH=$VERSION_PATH
+echo Using DEST=$DEST
+echo Using MANIFEST=$MANIFEST
+echo Using INSTALL_LOCATION=$INSTALL_LOCATION
 
 . ./dist.info.manual
 bash package.bin.std -local ${MANIFEST}
