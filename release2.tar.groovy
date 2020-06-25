60af48b196dbc099714d7735fa0d22d02e5e0c2e
Randall S. Becker <rsbecker@nexbridge.com>
2020-06-25 08:14:01 -0500

    Add pipeline variant of release groovy script

---
+++ release2.tar.groovy
@@ -0,0 +1,29 @@
+/**
+ * Packager used for gz and bz2 files where the name has the form:
+ *    package-version-release.tar.(gz|bz2)
+ * that is repackaged with a different name replacing release with
+ * a suffix. So this would work for:
+ *    curl-7.46.0-release.tar.gz
+ * shipping as
+ *    curl-7.46.0-nse.tar.gz
+ */
+import hudson.model.*;
+import java.sql.*;
+
+def package() {
+	ReleasePackageEngine engine = new ReleasePackageEngine();
+	JdbcLoader loader = new JdbcLoader();
+
+	String schema = System.getenv("SCHEMA");
+	engine.setSchema(schema);
+
+	Connection connection = loader.getConnection();
+	engine.setConnection(connection);
+
+	engine.execute();
+	connection.close();
+}
+
+return [
+	package: this.&package
+]
