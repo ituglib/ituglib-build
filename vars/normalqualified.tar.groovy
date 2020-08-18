2e55e5075c3340df0c23f6c2a95786652d2886aa
Randall S. Becker <rsbecker@nexbridge.com>
2020-08-18 09:40:59 -0500

    Copied normalqualified.tar into var structure

---
+++ vars/normalqualified.tar.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+/**
+ * Normal Qualified Packager script for direct use from a Pipeline.
+ *
+ * Packager used for gz and bz2 files where the name has the form:
+ *    package-version-sub-something.tar.(gz|bz2)
+ * that is repackaged with the same name. So this would work for:
+ *    sed-7.46.0-318f-src.tar.gz
+ */
+import hudson.model.*;
+import java.sql.*;
+import org.ituglib.deploy.*;
+
+def call(String package = 'human') {
+        echo "Hello, ${name}"
+        echo "Hello, "+GlobalVars.foo
+
+	//NormalQualifiedPackageEngine engine = new NormalQualifiedPackageEngine();
+	//JdbcLoader loader = new JdbcLoader();
+
+	//String schema = System.getenv("SCHEMA");
+	//engine.setSchema(schema);
+
+	//Connection connection = loader.getConnection();
+	//engine.setConnection(connection);
+
+	//engine.execute();
+	//connection.close();
+}
+
