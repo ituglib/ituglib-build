56fce69fed4ba1fcd524e2b509d223fe812fbfcf
Randall S. Becker <rsbecker@nexbridge.com>
2020-08-18 09:56:48 -0500

    Explicit reference of Abstracts

--- src/org/ituglib/deploy/NormalQualifiedPackageEngine.groovy
+++ src/org/ituglib/deploy/NormalQualifiedPackageEngine.groovy
@@ -4,6 +4,7 @@ import hudson.model.*;
 import java.io.*;
 import java.sql.*;
 import java.util.regex.*;
+import org.ituglib.deploy.AbstractPackageEngine;
 
 class NormalQualifiedPackageEngine extends AbstractPackageEngine {
 
