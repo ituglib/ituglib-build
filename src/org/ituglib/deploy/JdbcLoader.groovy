ba29b452541be7af8b4ddc115afd634b43dfc72a
Randall S. Becker <rsbecker@nexbridge.com>
2020-08-18 15:08:59 -0500

    Changed load technique

--- src/org/ituglib/deploy/JdbcLoader.groovy
+++ src/org/ituglib/deploy/JdbcLoader.groovy
@@ -9,26 +9,35 @@ import java.util.regex.*;
 
 class JdbcLoader {
 
-   static private final String JDBC_MX_TAR =
-      "/usr/tandem/jdbcMx/current/lib/jdbcMx.jar";
-   static private final String JDBC_DRIVER = 
-      "com.tandem.sqlmx.SQLMXDriver";
+	static private final String JDBC_MX_TAR =
+		"/usr/tandem/jdbcMx/current/lib/jdbcMx.jar";
+	static private final String JDBC_DRIVER = 
+		"com.tandem.sqlmx.SQLMXDriver";
 
-   public JdbcLoader() {
-   }
+	public JdbcLoader() {
+	}
 
-   public Connection getConnection() {
-      File localFile = new File(JDBC_MX_TAR);
-      echo "Attempting to use ${localFile}";
-      // Use the groovy script's classLoader to add the jar file at runtime.
-      this.class.classLoader.rootLoader.addURL(localFile.toURI().toURL());
+	public Connection getConnection() {
+		File localFile = new File(JDBC_MX_TAR);
+		if (localFile == null) {
+			echo "Could not load File object";
+			return null;
+		} else if (!localFile.exists()) {
+			echo JDBC_MX_TAR+" does not exist";
+			return null;
+		}
+		// Use the groovy script's classLoader to add the jar file at
+		// runtime.
+		this.class.classLoader.rootLoader.addURL(
+			localFile.toURI().toURL());
 
-      // Now register the driver.
-      Class.forName(JDBC_DRIVER);
+		// Now register the driver.
+		Class.forName(JDBC_DRIVER);
 
-      Connection connection = DriverManager.getConnection("jdbc:sqlmx:");
-      connection.setAutoCommit(false);
-      return connection;
-   }
+		Connection connection =
+			DriverManager.getConnection("jdbc:sqlmx:");
+		connection.setAutoCommit(false);
+		return connection;
+	}
 }
 
