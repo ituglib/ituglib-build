729ae2c2c601f9c92ac10b81ea5ccb69aba843ff
Randall S. Becker <randall.becker@nexbridge.ca>
2023-08-23 11:35:31 -0400

    maketar.sh: fix syntax and add diagnostics

--- maketar.sh
+++ maketar.sh
@@ -1,13 +1,23 @@
 #!/usr/coreutils/bin/bash
 
+PROGNAME=$0
+
 set -eo pipefail
 
-echo "Creating $2"
+exec 2>&1
+
+Usage() {
+	echo usage: $PROGNAME src-directory dest-archive
+	exit 1
+}
+
+echo "Creating $2 from $1:"
 ls -ld $1
 if [ "$PACKAGE_WITH_SUDO" = "true" ]; then
 	cd $1 && sudo tar czf $2 . 2>&1
 else
 	cd $1 && tar czf $2 . 2>&1
-endif
+fi
 
+echo "Archive:"
 ls -l $2
